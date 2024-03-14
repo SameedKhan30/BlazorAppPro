28deb490be6614268a97f1c3d4b8adb70855e246
msameed <sameed.salaar@gmail.com>
2024-03-14 06:31:34 -0400

    Add project files.

---
+++ BlazorAppPro/Client/Program.cs
@@ -0,0 +1,13 @@
+using BlazorAppPro.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+await builder.Build().RunAsync();
